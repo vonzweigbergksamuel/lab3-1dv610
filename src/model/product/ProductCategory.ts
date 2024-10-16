ec0b32ec39685a53bbf5d95978da6866ff978a3d
Samuel von Zweigbergk <sv222rr@student.lnu.se>
2024-10-16 11:10:01 +0200

    Product + category created

---
+++ src/model/product/ProductCategory.ts
@@ -0,0 +1,11 @@
+export enum ProductCategory {
+  Electronics = "Electronics",
+  Clothing = "Clothing",
+  Food = "Food",
+  Books = "Books",
+  Beauty = "Beauty",
+  Sports = "Sports",
+  Toys = "Toys",
+  Furniture = "Furniture",
+  Other = "Other",
+}
